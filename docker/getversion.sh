d56b7d2530d9d7363781fed03a2150f9ffe25172
krishna k maurya <krishna.maurya@paytmbank.com>
2021-08-04 19:20:31 +0530

    fix docker build-info path

--- docker/getversion.sh
+++ docker/getversion.sh
@@ -3,8 +3,8 @@
 . docker/utils.sh
 PROJECT_DIR="$( pwd )"
 
-if search $PROJECT_DIR/design-pattern/target/classes/META-INF/ build-info.properties;then
-  version=$(cat $PROJECT_DIR/design-pattern/target/classes/META-INF/build-info.properties | grep build.version | cut -d'=' -f2)
+if search $PROJECT_DIR//target/classes/META-INF/ build-info.properties;then
+  version=$(cat $PROJECT_DIR/target/classes/META-INF/build-info.properties | grep build.version | cut -d'=' -f2)
 else
   return 1
 fi
