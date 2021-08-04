18d85d0c9edb1f3a7533c1145da7a09f7550e745
krishna k maurya <krishna.maurya@paytmbank.com>
2021-08-04 19:39:05 +0530

    fix mvn deploy

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 . docker/utils.sh
 mvn clean install -U
+mvn deploy
 
-if commandExecutor  ". docker/getversion.sh";then
+if commandExecutor  ".
+docker/getversion.sh";then
   search './target/' "design-pattern.jar"
 else
   ret 1
