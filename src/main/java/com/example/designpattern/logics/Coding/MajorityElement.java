a1e0a38f9974c414369ed5fbceb0071b4e381b9e
krishna <krishna.maurya423@gmail.com>
2024-03-17 10:52:40 +0530

    Coding

---
+++ src/main/java/com/example/designpattern/logics/Coding/MajorityElement.java
@@ -0,0 +1,70 @@
+package com.example.designpattern.logics.Coding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MajorityElement {
+
+    public static List< Integer > majorityElement(int []v) {
+        // Write your code here
+        List<Integer> ans = new ArrayList<>();
+        int cnt1 =0;
+        int cnt2 =0;
+        int el1 = Integer.MIN_VALUE;
+        int el2 = Integer.MIN_VALUE;
+        int n = v.length;
+
+        if(n<3){
+            for (int i = 0; i < v.length; i++) {
+                ans.add(v[i]);
+            }
+            return ans;
+        }
+
+        for (int i = 0; i <n  ; i++) {
+            if(cnt1 == 0 && el2 != v[i]){
+                el1 = v[i];
+                cnt1 = 1;
+            } else if(cnt2 == 0){
+                el2 = v[i];
+                cnt2 =1;
+            } else if (v[i] == el1) {
+                cnt1++;
+            } else if (v[i] == el2) {
+                cnt2++;
+            }else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        int c1 = 0;
+        int c2 = 0;
+        for (int i = 0; i < v.length; i++) {
+            if(v[i]==el1){
+                c1++;
+            }
+            if(v[i]==el2){
+                c2++;
+            }
+        }
+
+        if(c1>n/3){
+            ans.add(el1);
+        }
+        if(c2>n/3){
+            ans.add(el2);
+        }
+        return ans;
+
+        //return new ArrayList<Integer>(Arrays.asList(el1, el2));
+    }
+
+
+    public static void main(String[] args) {
+        int a[] = {2, 2, 1, 3, 1, 2, 3, 1, 1};
+        System.out.println(majorityElement(a));
+    }
+
+}
---
+++ src/main/java/com/example/designpattern/logics/Coding/MissingAndRepeatingArrayElement.java
@@ -0,0 +1,37 @@
+package com.example.designpattern.logics.Coding;
+
+import java.util.Arrays;
+
+public class MissingAndRepeatingArrayElement {
+
+    public static int[] findMissingRepeatingNumbers(int []a) {
+        // Write your code here
+        long   n = a.length;
+        long  sum = (n*(n+1))/2;
+        long  sumSquare = (n*(n+1)*(2*n+1))/6;
+
+        long  arraySum = 0;
+        long  arraySumSquare = 0;
+        for ( int i = 0; i < n; i++) {
+
+            long element = Long.parseLong(String.valueOf(a[i]));
+            arraySum += element;
+            arraySumSquare += element*element;
+        }
+
+        long aMinusB = sum - arraySum;
+        long aSquareMinusBSquare = sumSquare - arraySumSquare;
+        long aPlusB = aSquareMinusBSquare/aMinusB;
+        long aVar = (aPlusB + aMinusB)/2;
+        long bVar = aVar - aMinusB;
+        // System.out.println(bVar + "  " + aVar);
+        return new int[]{(int)bVar,(int)aVar};
+    }
+
+    public static void main(String[] args) {
+
+        int []a = {1,1,3,2,4};
+        findMissingRepeatingNumbers(a);
+    }
+
+}
